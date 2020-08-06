8da524f5ba631532f1708a9d372b5474a8cd7b7c
margare-jihua <margaret.jihua@gmail.com>
2020-08-06 10:18:15 -0700

    Basic setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const ejsLayouts = require('express-ejs-layouts')
+
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+app.listen(8000)
